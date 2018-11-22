e5ba57ae7537364572b5f99097373f7eafcd79af
thinktothings <thinktothings@gmail.com>
2018-11-22 12:57:15 +0800

    RangeDependency 数据流转图和源码

--- src/main/scala/com/opensource/bigdata/spark/local/rdd/operation/dependency/narrow/n_02_RangeDependency/Run3.scala
+++ src/main/scala/com/opensource/bigdata/spark/local/rdd/operation/dependency/narrow/n_02_RangeDependency/Run3.scala
@@ -2,15 +2,15 @@ package com.opensource.bigdata.spark.local.rdd.operation.dependency.narrow.n_02_
 
 import com.opensource.bigdata.spark.local.rdd.operation.base.BaseScalaSparkContext
 
-object Run2 extends BaseScalaSparkContext{
+object Run3 extends BaseScalaSparkContext{
 
   def main(args: Array[String]): Unit = {
     val sc = pre()
-    val rdd1 = sc.textFile("/opt/data/2/a.txt",2)
+    val rdd1 = sc.textFile("/opt/data/2/c.txt",2)
+    val rdd2 = sc.textFile("/opt/data/2/a.txt",2)
+    val rdd3 = rdd1.union(rdd2)
 
-    println(rdd1.collect().mkString("\n"))
-
-    //rdd1.partitions(0).asInstanceOf[org.apache.spark.rdd.HadoopPartition]
+    println(rdd3.collect().mkString("\n"))
 
     sc.stop()
   }
