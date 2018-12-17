d3d1ab67014a6fc0d7cbbcd50c0503477462e0e3
thinktothings <thinktothings@gmail.com>
2018-12-17 18:31:49 +0800

    增加用户自定义聚合函数

--- src/main/scala/com/opensource/bigdata/spark/sql/n_02_spark_dataset/user_defined_untype_aggregation/n_01_uduta_MyAverage/Run.scala
+++ src/main/scala/com/opensource/bigdata/spark/sql/n_02_spark_dataset/user_defined_untype_aggregation/n_01_uduta_MyAverage/Run.scala
@@ -1,18 +1,49 @@
 package com.opensource.bigdata.spark.sql.n_02_spark_dataset.user_defined_untype_aggregation.n_01_uduta_MyAverage
 
 import com.opensource.bigdata.spark.standalone.base.BaseSparkSession
-import org.apache.spark.sql.expressions.{Aggregator, UserDefinedAggregateFunction}
-import org.apache.spark.sql.{Encoder, Encoders}
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.expressions.{ MutableAggregationBuffer, UserDefinedAggregateFunction}
+import org.apache.spark.sql.types._
 
 object Run extends BaseSparkSession{
 
-  case class Employee(name: String, salary: Long)
-  case class Average(var sum: Long, var count: Long)
 
 
   object MyAverage extends UserDefinedAggregateFunction{
 
+    //聚合函数的输入参数数据类型
+    def inputSchema: StructType = {
+      StructType(StructField("inputColumn",LongType) :: Nil)
+    }
 
+    def bufferSchema: StructType = {
+      StructType(StructField("sum",LongType) :: StructField("count",LongType) :: Nil)
+    }
+
+    def dataType: DataType = DoubleType
+
+    def deterministic: Boolean = true
+
+    def initialize(buffer: MutableAggregationBuffer): Unit = {
+      buffer(0) = 0L
+      buffer(1) = 0L
+    }
+
+
+    def update(buffer: MutableAggregationBuffer, input: Row): Unit ={
+      if(!input.isNullAt(0)){
+        buffer(0) = buffer.getLong(0) + input.getLong(0)
+        buffer(1) = buffer.getLong(1) + 1
+      }
+    }
+
+    def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit={
+      buffer1(0) = buffer1.getLong(0) +buffer2.getLong(0)
+      buffer1(1) = buffer1.getLong(1) +buffer2.getLong(1)
+    }
+
+
+    def evaluate(buffer: Row): Double = buffer.getLong(0).toDouble / buffer.getLong(1)
 
 
   }
@@ -21,22 +52,13 @@ object Run extends BaseSparkSession{
 
     val spark = sparkSession(true)
 
-    val ds = spark.read.json("hdfs://standalone.com:9000/home/liuwen/data/employees.json")
-    ds.show()
-//    +----+-------+
-//    | age|   name|
-//    +----+-------+
-//    |null|Michael|
-//    |  30|   Andy|
-//    |  19| Justin|
-//    +----+-------+
+    spark.udf.register("MyAverage",MyAverage)
 
-    val averageColumn = MyAverage.toColumn.name("平均值")
-    ds.select(averageColumn).show()
+    val df = spark.read.json("hdfs://standalone.com:9000/home/liuwen/data/employees.json")
+    df.createOrReplaceTempView("employees")
+    val sqlDF = spark.sql("select MyAverage(salary)  as average_slary from employees  ")
 
-
-
-
+    sqlDF.show()
 
     spark.stop()
   }
